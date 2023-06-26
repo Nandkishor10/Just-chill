3e0311a6dd5afb964f138a1af17429cbbfa1578e
Nandkishor10 <118745379+Nandkishor10@users.noreply.github.com>
2023-06-26 16:35:41 +0530

    Commit code

---
+++ asset/js/script.js
@@ -0,0 +1,35 @@
+/*const ages=[22,23,24,25,26,27,28,29,30];
+document.getElementById("Filter").innerHTML=ages.filter(checkAdult);
+function checkAdult(age){
+    return age>=18;
+}*/
+
+
+
+/*const array1 = [[0, 1], [2, 3], [4, 5]];
+const result = array1.reduceRight((accumulator, currentValue) => accumulator.concat(currentValue));
+document.getElementById("Filter").innerHTML=array1.filter(checkAdult);
+function checkAdult(_array1){
+    return true;
+}
+console.log([result]);
+// Expected output: Array [4, 5, 2, 3, 0, 1]
+
+const array11 = [0, 1, 2, 3, 4, 5];
+ let array111=array11.map(x=> x*2);
+ console.log(array111);
+
+
+ const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+/**
+ * Filter array items based on search criteria (query)
+
+function filterItems(arr, query) {
+  return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+
+console.log(filterItems(fruits, "ap")); // ['apple', 'grapes']
+console.log(filterItems(fruits, "ba")); // ['banana', 'mango', 'orange']
+*/
+
